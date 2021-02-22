e3a9d8ec1d81a905674379561461b7e7113ecb3c
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2021-02-22 17:10:49 +0900

    ぎゃふん

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -eu
 
-# docker.sock から gid を取得して、docker グループを作成し、
-# vscode ユーザーを所属させる
+# docker.sock から gid を取得して、docker グループの gid を変更
 docker_group_id=$(ls -n /var/run/docker.sock | cut -d ' ' -f 4)
-sudo groupadd --gid ${docker_group_id} docker
-sudo usermod -aG docker vscode
+sudo groupmod --gid ${docker_group_id} docker
 
 # 無限待ち（vscode デフォルトの entrypoint と同じ方法で）
 while sleep 1000; do :; done
